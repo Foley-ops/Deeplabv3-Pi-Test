8575e06ae64e3e1ab9b1fa74b6e5eadabcb4be1e
Foley-ops <nickmfoley@gmail.com>
2025-02-19 17:06:47 -0600

    modified reapeter.py to track time and give output

--- repeater.py
+++ repeater.py
@@ -1,4 +1,24 @@
 import subprocess
+import time
 
-for _ in range(10):
-    subprocess.run(["python3", "deeplabv3_single.py"])
+RUN_COMMAND = "python3"
+RUN_FILE = "deeplabv3_VOS.py"
+
+print(f"Starting Repeater on {RUN_FILE}...")
+for i in range(10):
+    start_time = time.time()
+    print(f"Run #{i+1}")
+    
+    subprocess.run([RUN_COMMAND, RUN_FILE])
+    
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    if elapsed_time > 60:
+        minutes = int(elapsed_time // 60)
+        seconds = elapsed_time % 60
+        print(f"Execution time: {minutes} minutes {seconds:.2f} seconds")
+    else:
+        print(f"Execution time: {elapsed_time:.2f} seconds")
+
+    print(" ")
